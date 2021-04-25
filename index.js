5acce806c2caa601c719d9051c01e30181294863
Kevin Benitez <kevinescudoderoble@gmail.com>
2021-04-25 20:03:53 -0300

    V.1

---
+++ index.js
@@ -0,0 +1,20 @@
+import './habmurger.css';
+
+export default function Hamburger (props){
+
+
+return (
+
+        <div className="nav_buton">
+            <label className="hamburger_buton">
+                <input type="checkbox" />
+                <span >
+                    <span ></span>
+                    <span ></span>
+                    <span ></span>
+                </span>
+            </label>
+        </div>
+)
+
+}
